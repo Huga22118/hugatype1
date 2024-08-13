e836a1c77933efad5cd96de5c4078aee82fd50b2
Huga <93875972+Huga22118@users.noreply.github.com>
2024-08-13 16:48:43 +0700

    Add files via upload

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #define PI 3.14
 
 using namespace std;
@@ -6,6 +7,10 @@ using namespace std;
 void persegipanjang(int p, int l)
 {
        int rumus = p * l;
+       cout << "Panjang: " << p;
+       cout << "\nLebar: " << l;
+       cout << "\nRumusnya adalah: Panjang x Lebar\n";
+       cout << p << "x" << l << "\n"; 
        cout << "Hasil dari Rumus Persegi Panjang Adalah: " << rumus;
        
 }
@@ -13,6 +18,9 @@ void persegipanjang(int p, int l)
 void kubus(int s)
 {
 	int rumus = s*s*s;
+	cout << "Sisi: " << s;
+    cout << "\nRumusnya adalah: Sisi x Sisi x Sisi\n";
+    cout << s << "x" << s << "x" << s << "\n"; 
 	cout << "Hasil dari Rumus Kubus Adalah: " << rumus;
 }
 
@@ -20,13 +28,21 @@ void lingkaran(int r)
 {
      float pi = 3.14;
      float rumus = (float)PI*r*r;
-     	cout << "Hasil dari Rumus Luas Lingkaran Adalah: " << rumus;
+    cout << "Jari-jari (r): " << r;
+    cout << "\nDiameter: " << r*r;
+    cout << "\nRumusnya adalah: Phi (3,14) x r x r\n";
+    cout << pi << "x" << r << "x" << r << "\n"; 
+   	cout << "Hasil dari Rumus Luas Lingkaran Adalah: " << rumus;
 }
 
 int main()
 {
+	 string sebutnamabang;
 	int pilihankang;
-	cout << "Pilih rumus:\n";
+	cout << "Tuliskan nama mu: ";
+	getline(cin, sebutnamabang);
+	cout << "Halo " << sebutnamabang;
+	cout << "\nPilih rumus:\n";
 	cout << "1.Luas Persegi Panjang\n";
 	cout << "2.Volume Kubus\n";
 	cout << "3.Luas Lingkaran\n";
